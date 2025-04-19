ceb7802aae512e2ce3a98315b9078505ed1fbe72
EruptedNuke <72616872+EruptedNuke@users.noreply.github.com>
2025-04-19 04:30:22 +0400

    Update main.lua
    fixed some crashing bugs and improved auto execute support

--- main.lua
+++ main.lua
@@ -1,19 +1,30 @@
 local plrservice = game:GetService("Players")
+
 local me = plrservice.LocalPlayer
 
 if not decompile then
- me:Kick("your executor doesnt has decompile function noob")
- return
+    me:Kick("your executor doesnt has decompile function noob")
+    return
 end
 
+local noobworkspace = game:GetService("Workspace")
+local noobreplicatedstorage = game:GetService("ReplicatedStorage")
+local noobreplicatedfirst = game:GetService("ReplicatedFirst")
+local noobstarterplayer = nil 
+local noobstartercharacter = nil
 
-local noobworkspace = game:GetService("Workspace"):GetDescendants()
-local noobreplicatedstorage = game:GetService("ReplicatedStorage"):GetDescendants()
-local noobreplicatedfirst = game:GetService("ReplicatedFirst"):GetDescendants()
-local noobstarterplayer = game:GetService("StarterPlayer").StarterPlayerScripts:GetDescendants()
-local nobostartercharacter = game:GetService("StarterPlayer").StarterCharacterScripts:GetDescendants()
-local noobstartergui = game:GetService("StarterGui"):GetDescendants()
-local noobnilinstances = getnilinstances()
+if game:GetService("StarterPlayer"):FindFirstChild("StarterPlayerScripts") then 
+    noobstarterplayer = game:GetService("StarterPlayer"):FindFirstChild("StarterPlayerScripts")
+end 
+
+if game:GetService("StarterPlayer"):FindFirstChild("StarterCharacterScripts") then 
+    noobstartercharacter = game:GetService("StarterPlayer"):FindFirstChild("StarterCharacterScripts")
+end 
+
+
+local noobstartergui = game:GetService("StarterGui")
+local noobnilinstances = getnilinstances() 
+
 
 local DecompileStartTime = os.time()
 local ScriptsFailedToDecompile = 0
@@ -24,16 +35,15 @@ local ScriptDumpingCompleted = false
 local ScriptDumperDirFolderName = tostring(game.PlaceId)
 
 if not isfolder(ScriptDumperDirFolderName) then
-makefolder(ScriptDumperDirFolderName)
+    makefolder(ScriptDumperDirFolderName)
 end
-
 local ServicesCrap = {
-["Nil_Instances"] = {DecompileState=true,Path=noobnilinstances},
-["StarterPlayerScripts"] = {DecompileState=true,Path=noobstarterplayer},
-["StarterCharacterScripts"] = {DecompileState=true,Path=nobostartercharacter},
-["ReplicatedStorage"] = {DecompileState=true,Path=noobreplicatedstorage},
-["ReplicatedFirst"] = {DecompileState=true,Path=noobreplicatedfirst},
-["StarterGui"] = {DecompileState=true,Path=noobstartergui}
+    ["StarterPlayerScripts"]={DecompileState=true,Path=noobstarterplayer},
+    ["StarterCharacterScripts"]={DecompileState=true,Path=nobostartercharacter},
+    ["ReplicatedStorage"]={DecompileState=true,Path=noobreplicatedstorage},
+    ["ReplicatedFirst"]={DecompileState=true,Path=noobreplicatedfirst},
+    ["StarterGui"]={DecompileState=true,Path=noobstartergui},
+    ["Nil_Instances"]={DecompileState=true,Path=noobnilinstances}
 }
 
 function DecompileScriptGrr(ScriptToDecompile)
@@ -41,39 +51,55 @@ function DecompileScriptGrr(ScriptToDecompile)
     local DecompiledOutput = nil 
     local ScriptByteCode = nil 
 
-    Success, ReturnedData =
-        pcall(function()
-            ScriptByteCode = getscriptbytecode(ScriptToDecompile)
-            if ScriptByteCode and string.len(ScriptByteCode)>0 then
-                DecompiledOutput = decompile(ScriptToDecompile)
-            end
-        end)
+    Success, ReturnedData = pcall(function()
+        ScriptByteCode = getscriptbytecode(ScriptToDecompile)
+        if ScriptByteCode and string.len(ScriptByteCode) > 0 then
+             DecompiledOutput = decompile(ScriptToDecompile)
+         end
+    end)
+
     return {
         ["Success"] = Success,
         ["Output"] = DecompiledOutput or "Unknown Bytecode"
     }
 end
 
+local function SafeDecompileScript(script, Category)
+    local ScriptDecompiled = DecompileScriptGrr(script)
+    if ScriptDecompiled.Success and ScriptDecompiled.Output ~= "Unknown Bytecode" then
+        ScriptsSuccesfullyDecompiled = ScriptsSuccesfullyDecompiled + 1
+        writefile(ScriptDumperDirFolderName .. "/" .. Category .. "/" .. script.Name .. "_" .. tostring(ScriptsSuccesfullyDecompiled) .. ".lua", ScriptDecompiled.Output)
+    else
+        ScriptsFailedToDecompile = ScriptsFailedToDecompile + 1
+    end
+end 
+
+local function DumpServiceScripts(Path,ServiceName)
+    for i, v in pairs(Path) do
+        if v:IsA("ModuleScript") or v:IsA("LocalScript") then
+            SafeDecompileScript(v,ServiceName)
+        end
+    end
+end
+
 task.spawn(function()
-for i, v in pairs(ServicesCrap) do
-    if v.DecompileState == true  then
-        makefolder(ScriptDumperDirFolderName .. "/" .. i)
-        for i2, v2 in pairs(v.Path) do
-            if v2:IsA("ModuleScript") or v2:IsA("LocalScript") then
-                local ScriptDecompiled = DecompileScriptGrr(v2)
-                if ScriptDecompiled.Success and ScriptDecompiled.Output~="Unknown Bytecode" then
-                    ScriptsSuccesfullyDecompiled = ScriptsSuccesfullyDecompiled+1
-                    writefile(ScriptDumperDirFolderName .. "/" .. i .. "/" .. v2.Name .. "_" .. tostring(i2) .. ".lua",ScriptDecompiled.Output)
-                else
-                    ScriptsFailedToDecompile = ScriptsFailedToDecompile + 1
+    for i, v in pairs(ServicesCrap) do
+        if v.DecompileState == true then
+            makefolder(ScriptDumperDirFolderName .. "/" .. i)
+            if v.Path ~= nil then
+                if typeof(v.Path) == "Instance" then
+                    DumpServiceScripts(v.Path:GetDescendants(), i)
+                elseif typeof(v.Path) == "table" then
+                    DumpServiceScripts(v.Path, i)
                 end
             end
         end
     end
-  end
 
-  ScriptDumpingCompleted = true 
+    ScriptDumpingCompleted = true
 end)
+
+
 
 repeat task.wait() until ScriptDumpingCompleted == true 
 
